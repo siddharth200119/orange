f9cf6d52e79aba29164772c7037e0bde3a9f4284
IronSpidy001 <74776640+IronSpidy001@users.noreply.github.com>
2022-10-05 23:32:44 +0530

    navbar added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var navBarList = ["Store","oPhone", "Mac", "oPad", "Watch", "AirPods", "TV", "Accessories", "Support"]
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.render("home");
+    res.render("home", {navBarList: navBarList});
 })
 
 app.listen("3000", function () {
