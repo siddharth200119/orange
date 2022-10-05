0ef287ef910b8b944873c8110752a5498f20ffc4
IronSpidy001 <74776640+IronSpidy001@users.noreply.github.com>
2022-10-06 01:01:39 +0530

    cards

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ const bodyParser = require("body-parser");
 
 var navBarList = ["Store","oPhone", "Mac", "oPad", "Watch", "AirPods", "TV", "Accessories", "Support"]
 
+var cards = [
+    {
+        title: "AirPods",
+        subTitle: "A better way to listen",
+        image: "images/airpods.png"
+    },
+    {
+        title: "MacBook",
+        subTitle: "Only Laptop you will need",
+        image: "images/mac.png"
+    },
+    {
+        title: "oPad",
+        subTitle: "Also a Laptop",
+        image: "images/IPad.png"
+    },
+    {
+        title: "HomePod",
+        subTitle: "Your only friend",
+        image: "images/homePod.png"
+    }
+]
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.render("home", {navBarList: navBarList});
+    res.render("home", {navBarList: navBarList, cards: cards});
 })
 
 app.listen("3000", function () {
